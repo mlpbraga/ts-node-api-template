6b3f11de13b909079235f6187338d5be9a598f95
Luísa Braga <mlpb@icomp.ufam.edu.br>
2020-06-23 00:11:11 -0400

    feat: users bith date

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
+
 const usersRouter = Router();
 
 const users = [];
 
 usersRouter.post('/', (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, birthDate } = req.body;
+
   const user = {
     id: uuid(),
     name,
     email,
     password,
-  }
+    birthDate: startOfHour(parseISO(birthDate)),
+  };
   users.push(user);
   return res.json(user);
 });
