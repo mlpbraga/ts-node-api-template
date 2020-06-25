229ac54ce76c98a3bb0467e98396241f776c2703
Luísa Braga <mlpb@icomp.ufam.edu.br>
2020-06-24 22:38:29 -0400

    feat: sessions route

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+  try {
+    const authenticateUserService = new AuthenticateUserService();
+    const { email, password } = req.body;
+    const { user } = await authenticateUserService.execute({
+      email,
+      password,
+    });
+    delete user.password;
+    return res.json({ user });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+export default sessionsRouter;
